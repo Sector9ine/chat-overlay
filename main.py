2eb263eb68ea9694ea1e930c85baf774b25e02d1
Sector9ine <johnjohnson8888888888888888@gmail.com>
2025-07-27 19:59:45 +0700

    update

--- main.py
+++ main.py
@@ -66,6 +66,6 @@ def start_background_thread():
         thread.start()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
 
 
